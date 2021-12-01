7dd33ca18e978043a0b7578c730c05ce9292f9ef
Krzysztof Zawadka <krzysztof.zawadka@pl.markant.com>
2021-12-01 12:57:11 +0100

    Code cleanup, function names changed

--- day01.py
+++ day01.py
@@ -1,21 +1,17 @@
-def calcuateIncreases(depths):
+def countIncreases(depths):
     increasesCount = 0
     for x, y in zip(depths, depths[1:]):
         increasesCount += x < y
-
     return increasesCount
 
-
-def calculateIncresaingSums(depths):
+def countIncreasingSums(depths):
     sums = []
     for x, y, z in zip(depths, depths[1:], depths[2:]):
         sums.append(x + y + z)
-
-    return calcuateIncreases(sums)
-
+    return countIncreases(sums)
 
 depths = [int(x) for x in open("day01.txt", "r")]
 test = [199, 200, 208, 210, 200, 207, 240, 269, 260, 263]
 
-print("Part 1:", calcuateIncreases(depths))
-print("Part 2:", calculateIncresaingSums(depths))
+print("Part 1:", countIncreases(depths))
+print("Part 2:", countIncreasingSums(depths))
