96accee8b334eae59c8f18a6efebdcd42fde7973
Krzysztof Zawadka <krzysztof.zawadka@pl.markant.com>
2021-12-02 08:09:10 +0100

    Day 2 solutions

---
+++ day02.py
@@ -0,0 +1,54 @@
+def countPositionAndDepth(movement):
+    position = 0
+    depth = 0
+
+    for command, change in movement:
+        if command == "forward":
+            position += change
+        elif command == "down":
+            depth += change
+        elif command == "up":
+            depth -= change
+
+    return (position, depth)
+
+
+def countPositionAndDepthWithAiming(movement):
+    position = 0
+    depth = 0
+    aim = 0
+
+    for command, change in movement:
+        if command == "down":
+            aim += change
+        elif command == "up":
+            aim -= change
+        elif command == "forward":
+            position += change
+            depth += change * aim
+
+    return (position, depth)
+
+
+inputFile = []
+test = [
+    ("forward", 5),
+    ("down", 5),
+    ("forward", 8),
+    ("up", 3),
+    ("down", 8),
+    ("forward", 2),
+]
+
+with open("day02.txt", "r") as file:
+    lines = file.readlines()
+    lines = [line.rstrip() for line in lines]
+
+for line in lines:
+    command, size = line.split(" ")
+    inputFile.append((command, int(size)))
+
+(pos, dep) = countPositionAndDepth(inputFile)
+print(f"Part 1: {pos*dep} (position = {pos}, depth = {dep})")
+(pos, dep) = countPositionAndDepthWithAiming(inputFile)
+print(f"Part 2: {pos*dep} (position = {pos}, depth = {dep})")
