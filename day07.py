d76eccc7b98f226e8f32c9199f41a7d781200d28
Krzysztof Zawadka <krzysztof.zawadka@pl.markant.com>
2021-12-07 09:22:53 +0100

    Improved day 7 solution

--- day07.py
+++ day07.py
@@ -1,32 +1,22 @@
 def count_fuel(crabs, i):
     total_fuel = 0
-    for crab in crabs:
-        total_fuel += abs(crab - i)
-    return total_fuel
-
-
-def count_increasing_fuel(crabs, i):
-    total_fuel = 0
+    total_increasing_fuel = 0
     for crab in crabs:
         steps = abs(crab - i)
-        total_fuel += int(((steps + 1) * steps) / 2)
-    return total_fuel
+        total_fuel += steps
+        total_increasing_fuel += int(((steps + 1) * steps) / 2)
+    return total_fuel, total_increasing_fuel
 
 
-def part_one(crabs):
+def solution(crabs):
     min_value, max_value = min(crabs), max(crabs)
     possible_fuels = []
+    possible_increasing_fuels = []
     for i in range(min_value, max_value + 1):
-        possible_fuels.append(count_fuel(crabs, i))
-    return min(possible_fuels)
-
-
-def part_two(crabs):
-    min_value, max_value = min(crabs), max(crabs)
-    possible_fuels = []
-    for i in range(min_value, max_value + 1):
-        possible_fuels.append(count_increasing_fuel(crabs, i))
-    return min(possible_fuels)
+        normal, increasing = count_fuel(crabs, i)
+        possible_fuels.append(normal)
+        possible_increasing_fuels.append(increasing)
+    return min(possible_fuels), min(possible_increasing_fuels)
 
 
 test = [16, 1, 2, 0, 4, 2, 7, 1, 2, 14]
@@ -34,5 +24,5 @@ with open("data/day07.txt", "r") as f:
     input = f.read().strip()
 input = list(map(int, input.split(",")))
 
-print(part_one(input[:]))
-print(part_two(input[:]))
+part_one, part_two = solution(input)
+print(f"Part 1: result = {part_one}\nPart 2: result = {part_two}")
