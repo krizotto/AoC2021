d65b9e90bb1a6d7079783e24ab76a56493e73d5a
zawadka <kzawadka@gmail.com>
2021-12-12 01:31:40 +0100

    Day 11 indicies solution

--- day11.py
+++ day11.py
@@ -1,45 +1,54 @@
 import time
+import numpy
 from copy import deepcopy
-from itertools import product
 
 
-def flash(pos, neighbors, octopuses, flash_count=0):
+def check_neighbors(x, y):
+    possible_neighbors = []
+    # check row above
+    if y - 1 >= 0:
+        possible_neighbors.append((x, y - 1))
+        if x - 1 >= 0:
+            possible_neighbors.append((x - 1, y - 1))
+        if x + 1 < 10:
+            possible_neighbors.append((x + 1, y - 1))
+    # check row below
+    if y + 1 < 10:
+        possible_neighbors.append((x, y + 1))
+        if x - 1 >= 0:
+            possible_neighbors.append((x - 1, y + 1))
+        if x + 1 < 10:
+            possible_neighbors.append((x + 1, y + 1))
+    # check my row
+    if x - 1 >= 0:
+        possible_neighbors.append((x - 1, y))
+    if x + 1 < 10:
+        possible_neighbors.append((x + 1, y))
+    return possible_neighbors
+
+
+def flash(x, y, octopuses, flash_count=0):
     flash_count += 1
-    octopuses[pos] = 0
-    for n in neighbors:
-        if octopuses[n] == 0:
+    octopuses[y][x] = 0
+    for nx, ny in check_neighbors(x, y):
+        if octopuses[ny][nx] == 0:
             continue
-        octopuses[n] += 1
-        if octopuses[n] > 9:
-            flash_count = flash(n, find_neighbors(n, octopuses), octopuses, flash_count)
+        octopuses[ny][nx] += 1
+        if octopuses[ny][nx] > 9:
+            flash_count = flash(nx, ny, octopuses, flash_count)
     return flash_count
-
-
-def find_neighbors(pos, octopuses):
-    return [
-        n
-        for n in [
-            (pos[0] - 1, pos[1] - 1),
-            (pos[0] - 1, pos[1]),
-            (pos[0] - 1, pos[1] + 1),
-            (pos[0], pos[1] - 1),
-            (pos[0], pos[1] + 1),
-            (pos[0] + 1, pos[1] - 1),
-            (pos[0] + 1, pos[1]),
-            (pos[0] + 1, pos[1] + 1),
-        ]
-        if n in octopuses
-    ]
 
 
 def step(octopuses):
     flash_count = 0
-    for octo in octopuses:
-        octopuses[octo] += 1
+    for y in range(10):
+        for x in range(10):
+            octopuses[y][x] += 1
 
-    for pos, val in octopuses.items():
-        if val > 9:
-            flash_count += flash(pos, find_neighbors(pos, octopuses), octopuses)
+    for y in range(10):
+        for x in range(10):
+            if octopuses[y][x] > 9:
+                flash_count += flash(x, y, octopuses)
     return flash_count
 
 
@@ -51,17 +60,19 @@ def part_two(octopuses):
     steps = 0
     while True:
         steps += 1
-        if step(octopuses) == len(octopuses):
+        if step(octopuses) == 100:
             return steps
 
 
-
 start_time = time.time()
-with open("data/day11.txt", "r") as file:
-    data = [list(map(int, list(i.rstrip()))) for i in  file]
-cords = list(product(range(10), range(10)))
-octo = {c: data[c[1]][c[0]] for c in cords}
 
-print("Part 1: result = ", part_one(deepcopy(octo)))
-print("Part 2: result = ", part_two(octo))
+octopuses = []
+with open("data/day11.txt", "r") as file:
+    lines = file.readlines()
+    for line in lines:
+        octopuses.extend(int(x) for x in list(line.strip()))
+octopuses = numpy.array(octopuses).reshape((10, 10))
+
+print("Part 1: result = ", part_one(deepcopy(octopuses)))
+print("Part 2: result = ", part_two(octopuses))
 print("--- %s seconds ---" % (time.time() - start_time))
