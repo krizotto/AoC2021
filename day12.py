5edaf5bdad379f5e880dfc91dbe8403aface14bf
zawadka <kzawadka@gmail.com>
2021-12-12 11:29:37 +0100

    Small code reformatting

--- day12.py
+++ day12.py
@@ -14,9 +14,10 @@ def part_one(graph, node="start", path=[]):
             path_count += part_one(graph, child, path[:])
     return path_count
 
+
 def part_two(graph, node="start", path=[], is_double_used=False):
     path_count = 0
-    if node.islower() and node in path and (is_double_used or node in ["start","end"]):
+    if node.islower() and node in path and (is_double_used or node in ["start", "end"]):
         return path_count
     path.append(node)
     if node.islower() and path.count(node) == 2:
@@ -24,22 +25,22 @@ def part_two(graph, node="start", path=[], is_double_used=False):
     if "end" in path:
         return 1
     for child in graph[node]:
-        if not child.islower() or child not in path or (not is_double_used and node not in ["start","end"]):
+        if (
+            not child.islower()
+            or child not in path
+            or (not is_double_used and node not in ["start", "end"])
+        ):
             path_count += part_two(graph, child, path[:], is_double_used)
     return path_count
 
 
 start_time = time.time()
-paths = []
-with open("data/day12.txt", "r") as f:
-    lines = f.readlines()
-    for line in lines:
-        a, b = line.strip().split("-")
-        paths.append((a, b))
-        paths.append((b, a))
 graph = defaultdict(list)
-for k, v in paths:
-    graph[k].append(v)
+with open("data/day12.txt", "r") as f:
+    for line in f.readlines():
+        a, b = line.strip().split("-")
+        graph[a].append(b)
+        graph[b].append(a)
 
 print(f"Part 1: result = {part_one(graph)}")
 print(f"Part 2: result = {part_two(graph)}")
