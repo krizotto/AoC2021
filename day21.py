e4174bdac91f055948ca3a06c626e4b8195e70e3
Krzysztof Zawadka <krzysztof.zawadka@pl.markant.com>
2021-12-21 15:17:43 +0100

    Removed unnecessary code, small refactoring

--- day21.py
+++ day21.py
@@ -6,19 +6,19 @@ class Die:
         self.number = number
         self.times_rolled = 0
 
-    def throw(self) -> int:
+    def throw(self) -> list:
         roll1 = (self.number) % 100 + 1
         roll2 = (self.number + 1) % 100 + 1
         roll3 = (self.number + 2) % 100 + 1
         self.number = roll3
         self.times_rolled += 3
-        return roll1 + roll2 + roll3
+        return [roll1, roll2, roll3]
 
 
 class Player:
-    def __init__(self, pos: int) -> None:
+    def __init__(self, pos: int, score: int = 0) -> None:
         self.pos = pos
-        self.score = 0
+        self.score = score
 
     def move(self, move: int) -> None:
         self.pos = (self.pos + move - 1) % 10 + 1
@@ -27,10 +27,10 @@ class Player:
 
 def part_one(die: Die, p1: Player, p2: Player):
     while True:
-        p1.move(die.throw())
+        p1.move(sum(die.throw()))
         if p1.score >= 1000:
             break
-        p2.move(die.throw())
+        p2.move(sum(die.throw()))
         if p2.score >= 1000:
             break
     return min(p1.score, p2.score) * die.times_rolled
@@ -40,14 +40,6 @@ start_time = time.time()
 die = Die()
 p1 = Player(7)
 p2 = Player(4)
-
-while True:
-    p1.move(die.throw())
-    if p1.score >= 1000:
-        break
-    p2.move(die.throw())
-    if p2.score >= 1000:
-        break
 
 print("Part 1: result = ", part_one(die, p1, p2))
 print("--- %s seconds ---" % (time.time() - start_time))
