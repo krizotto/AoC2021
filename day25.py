3d5785c8b156e24c488f36f8442b4793ba03478f
zawadka <kzawadka@gmail.com>
2021-12-25 20:43:37 +0100

    Optimized solution

--- day25.py
+++ day25.py
@@ -1,79 +1,60 @@
 import time
 import numpy as np
+import os
 
 
-def get_next_index(current, range):
-    next_index = current + 1
-    if next_index >= range:
-        next_index -= range
-    return next_index
+def clear():
+    return os.system("clear")
 
 
-def move_right(numbers: np.ndarray, horizontal: int, vertical: int):
-    moved_to = []
-    moved_from = []
-    for y in range(vertical):
-        for x in range(horizontal):
-            if (x, y) in moved_to or numbers[y, x] != ">":
-                continue
-            nx = (x + 1) % horizontal
-            if numbers[y, nx] != "v" and numbers[y, nx] != ">" and (nx, y) not in moved_from:
-                moved_to.append((nx, y))
-                moved_from.append((x, y))
-    for (x, y) in moved_from:
-        numbers[y, x] = "."
-    for (x, y) in moved_to:
-        numbers[y, x] = ">"
-    return numbers, len(moved_to) > 0
+def print_array(numbers: np.ndarray):
+    return print("\n".join(["".join([str(cell) for cell in row]) for row in numbers]))
 
 
-def move_down(numbers: np.ndarray, horizontal: int, vertical: int):
-    moved_to = []
-    moved_from = []
-    for y in range(vertical):
-        for x in range(horizontal):
-            if (x, y) in moved_to or numbers[y, x] != "v":
-                continue
-            ny = (y + 1) % vertical
-            if numbers[ny, x] != "v" and numbers[ny, x] != ">" and (x, ny) not in moved_from:
-                moved_to.append((x, ny))
-                moved_from.append((x, y))
-    for (x, y) in moved_from:
-        numbers[y, x] = "."
-    for (x, y) in moved_to:
-        numbers[y, x] = "v"
-    return numbers, len(moved_to) > 0
+def process(numbers: np.ndarray, direction: str, axis: int):
+    not_empty = numbers != "."
+    dest_elements = numbers == direction
+
+    all_moved = np.roll(dest_elements, axis=axis, shift=1)  # yet not checked if landed on proper spot
+    dest_to = all_moved & ~not_empty  # find every moved that landed on "."
+    numbers[dest_to] = direction  # change to new value
+    dest_from = np.roll(dest_to, axis=axis, shift=-1)  # find the origin of every correctly moved
+    numbers[dest_from] = "."  # make origin positions "."
+
+    return dest_to, numbers
 
 
-def part_one(numbers: np.ndarray, ysize: int, xsize: int):
+def part_one(numbers: np.ndarray, show_steps: bool):
     steps = 0
+    if show_steps:
+        clear()
+        print(steps)
+        print_array(numbers)
+        time.sleep(1)
     while True:
         steps += 1
-        print(steps)
-        do_repeat = False
-        numbers, has_moved = move_right(numbers, ysize, xsize)
-        # print(numbers)
-        do_repeat = do_repeat or has_moved
-        numbers, has_moved = move_down(numbers, ysize, xsize)
-        # print(numbers)
-        do_repeat = do_repeat or has_moved
-        if not do_repeat:
+        # EAST
+        moved_east, numbers = process(numbers, ">", 1)
+        # SOUTH
+        moved_south, numbers = process(numbers, "v", 0)
+
+        total_moves = np.sum(moved_east + moved_south)
+        if show_steps:
+            clear()
+            print(f"Step: {steps}. Moved: {total_moves}")
+            print_array(numbers)
+            time.sleep(0.15)
+        if total_moves == 0:
             break
     return steps
 
 
 start_time = time.time()
+show_steps = False
 numbers = []
-with open("data/day25.txt", "r") as f:
-    lines = f.readlines()
-    vertical = len(lines)
-    horizontal = len(lines[0].strip())
-    for line in lines:
-        numbers.extend(x for x in list(line.strip()))
-numbers = np.array(numbers).reshape((vertical, horizontal))
-
-print(numbers)
-print(part_one(numbers, horizontal, vertical))
-# print(f"Part 1: result = {solution(numbers)}")
-# print(f"Part 2: result = {solution(extend(numbers))}")
+with open("data/day25.txt") as f:
+    for line in f.readlines():
+        numbers.append([x for x in line.rstrip()])
+    numbers = np.array(numbers)
+print("Part 1: result = ", part_one(numbers, show_steps))
 print("--- %s seconds ---" % (time.time() - start_time))
